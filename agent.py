f07af78bca1748f6a77ca87155217d5823272105
Lavrenov Nick <kolya@lavrenov.net>
2018-07-03 11:49:44 +0300

    Random agent

--- agent.py
+++ agent.py
@@ -1,9 +1,44 @@
+from random import random, randrange
+
 
 class RandomAgent(object):
     """The world's simplest agent!"""
     def __init__(self, action_space):
         self.action_space = action_space
 
-    def act(self, observation, reward, done):
+    def act(self, observation):
         return self.action_space.sample()
 
+    def update_memory(self, observation, action, reward, next_observation, done):
+        pass
+
+class QLearningAgent(object):
+    """Agent with Q-table"""
+    def __init__(self, action_space):
+        self.action_space = action_space.n
+        self.memory = dict()  # Q
+
+        self.LR = 0.5
+        self.DF = 0.99
+        self.RF = 0.5
+
+    def act(self, observation):
+        if random() < self.RF:
+            return randrange(self.action_space)
+        self._fill_memory(observation)
+        action = 0
+        for i in range(self.action_space):
+            if self.memory[observation][i] > self.memory[observation][action]:
+                action = i
+        return action
+
+    def _fill_memory(self, value):
+        if value not in self.memory:
+            self.memory[value] = [random() * 2 - 1 for i in range(self.action_space)]
+
+    def update_memory(self, observation, action, reward, next_observation, done):
+        self._fill_memory(observation)
+        self._fill_memory(next_observation)
+
+        self.memory[observation][action] = (1 - self.LR) * self.memory[observation][action] + self.LR * \
+            (reward + self.DF * max([self.memory[next_observation][i] for i in range(self.action_space)]))
--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from taxi import TaxiEnv
-from agent import RandomAgent as Agent
-
+from agent import QLearningAgent as Agent
+# from agent import RandomAgent as Agent
 
 class RunAgent():
     def __init__(self):
         self.env = TaxiEnv()
         self.agent = Agent(self.env.action_space)
-        self.max_step = 1000
+        self.max_step = 100000
         self.render = True
 
     def do_episode(self):
@@ -16,9 +16,12 @@ class RunAgent():
         for step_i in range(self.max_step):
             if self.render:
                 self.env.render()
-            action = self.agent.act(observation, reward, done)
+            action = self.agent.act(observation)
             next_observation, reward, done, _ = self.env.step(action)
+            self.agent.update_memory(observation, action, reward, next_observation, done)
             amount += 1
+            if reward > 0:
+                print(reward)
             if done:
                 break
         return amount
@@ -27,4 +30,10 @@ class RunAgent():
 
 
 ra = RunAgent()
-print('total steps', ra.do_episode())
+ra.render = False
+i = 0
+while True:
+    print('Epoh {}; total steps: {}; LR {}; RF {}'.format(i, ra.do_episode(), ra.agent.LR, ra.agent.RF))
+    ra.agent.RF *= 0.999
+    ra.agent.LR *= 0.999
+    i += 1
