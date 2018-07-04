9c99769594366246274bef9f633b917b94cb53c4
Lavrenov Nick <kolya@lavrenov.net>
2018-07-04 18:28:22 +0300

    remove taxi and extra code, PEP8 compliance

--- agent.py
+++ agent.py
@@ -1,52 +1,11 @@
-from random import random, randrange, choice
+from random import random, choice
 import math
 import pickle
-
-class RandomAgent(object):
-    """The world's simplest agent!"""
-    def __init__(self, action_space):
-        self.action_space = action_space
-
-    def act(self, observation):
-        return self.action_space.sample()
-
-    def update_memory(self, observation, action, reward, next_observation, done):
-        pass
-
-class QLearningAgent(object):
-    """Agent with Q-table"""
-    def __init__(self, action_space):
-        self.action_space = action_space.n
-        self.memory = dict()  # Q
-
-        self.LR = 0.5
-        self.DF = 0.99
-        self.RF = 0.5
-
-    def act(self, observation):
-        if random() < self.RF:
-            return randrange(self.action_space)
-        self._fill_memory(observation)
-        action = 0
-        for i in range(self.action_space):
-            if self.memory[observation][i] > self.memory[observation][action]:
-                action = i
-        return action
-
-    def _fill_memory(self, value):
-        if value not in self.memory:
-            self.memory[value] = [random() * 2 - 1 for i in range(self.action_space)]
-
-    def update_memory(self, observation, action, reward, next_observation, done):
-        self._fill_memory(observation)
-        self._fill_memory(next_observation)
-
-        self.memory[observation][action] = (1 - self.LR) * self.memory[observation][action] + self.LR * \
-            (reward + self.DF * max([self.memory[next_observation][i] for i in range(self.action_space)]))
+from collections import defaultdict
 
 
 class MAXQAgent(object):
-    def __init__(self, action_space, log = False):
+    def __init__(self, action_space, log=False):
         self.action_space = action_space.n
         self.log = log
 
@@ -56,37 +15,34 @@ class MAXQAgent(object):
 
         # V[(Node ID, observation)] -> the expected cumulative reward of executing Node
         # starting in state observatioin until Node terminates.
-        self.V = dict()
+        self.V = defaultdict(lambda: 0)
 
         # C[(Node ID, observation, Action (node ID))] -> the expected discounted cumulative
         # reward of completing subtask Node after invoking the subroutine for subtask Action in state observation
-        self.C = dict()
+        self.C = defaultdict(lambda: 0)
 
         self.init_nodes()
 
-    def save_memory(self, filename = 'memory.pickle'):
-        pickle.dump([self.V, self.C], open(filename, 'wb'))
+    def save_memory(self, filename='memory.pickle'):
+        pickle.dump([dict(self.V), dict(self.C)], open(filename, 'wb'))
 
-    def load_memory(self, filename = 'memory.pickle'):
+    def load_memory(self, filename='memory.pickle'):
         try:
-            self.V, self.C = pickle.load(open(filename, 'rb'))
+            self.V, self.C = [defaultdict(lambda: 0, i) for i in  pickle.load(open(filename, 'rb'))]
         except Exception as e:
             print(str(e), 'in load memory')
 
     def getQ(self, node, observation, action):
-        if (node.id, observation, action.id) not in self.C:
-            self.C[(node.id, observation, action.id)] = -1
-        return self.getV(action, observation) + self.C[(node.id, observation, action.id)]
+        return self.getV(action, observation) + self.C[node.id, observation, action.id]
 
     def getV(self, node, observation):
         if node.is_primitive():
-            if (node.id, observation) not in self.V:
-                self.V[(node.id, observation)] = -1
-            return self.V[(node.id, observation)]
+            return self.V[node.id, observation]
         else:
-            return max([self.getQ(node, observation, node.childrens[child_id]) for child_id in node.childrens])
+            return max([self.getQ(node, observation, node.children[child_id]) for child_id in node.children])
 
-    def taxiDecode(self, i):
+    @staticmethod
+    def taxiDecode(i):
         out = []
         out.append(i % 4)
         i = i // 4
@@ -99,77 +55,101 @@ class MAXQAgent(object):
         return list(reversed(out))
 
     def check_navigate(self, destination):
+        """
+        returns function which check that current position is equal to destination
+        """
         locx, locy = [(0, 0), (0, 4), (4, 0), (4, 3)][destination]
+
         def func(observation):
-            decoded = self.taxiDecode(observation)
+            decoded = MAXQAgent.taxiDecode(observation)
             return decoded[0] == locx and decoded[1] == locy
 
         return func
 
-    def check_pickup(self):
-        def func(observation):
-            decoded = self.taxiDecode(observation)
-            return decoded[2] == 4
-        return func
+    @staticmethod
+    def check_pickup(observation):
+        """
+        Checks whether there was a pickup
+        """
+        decoded = MAXQAgent.taxiDecode(observation)
+        return decoded[2] == 4
 
-    def check_dropoff(self):
-        def func(observation):
-            decoded = self.taxiDecode(observation)
-            return decoded[2] != 4
-        return func
+    @staticmethod
+    def check_dropoff(observation):
+        """
+        Checks whether there was a dropoff
+        """
+        decoded = MAXQAgent.taxiDecode(observation)
+        return decoded[2] != 4
 
-    def check_done(self):
-        def func(observation):
-            return False
-        return func
+    def check_done(observation):
+        return False
 
     class Node:
-        def __init__(self, agent, action, id, check_T, childrens = None):
+        """
+        Node is vertex in graph of subtask.
+        """
+
+        def __init__(self, agent, action, id, check_T, children=None):
             self.agent = agent
             self.id = id
             self.action = action
             self.T = check_T
-            if childrens is None:
-                self.childrens = dict()
+            if children is None:
+                self.children = dict()
             else:
-                self.childrens = {i.id: i for i in childrens}
+                self.children = {i.id: i for i in children}
 
         def add_childer(self, child):
-            self.childrens[child.id] = child
+            self.children[child.id] = child
 
         def is_primitive(self):
-            return len(self.childrens) == 0
+            return len(self.children) == 0
 
         def choose_child(self, observation):
+            """
+            Return a child according eps-greedy policy
+
+            :param observation: current state
+            :return: One of the children (type Node)
+            """
             if random() < self.agent.RF:
-                return self.childrens[choice(list(self.childrens.keys()))]
+                return self.children[choice(list(self.children.keys()))]
             res = None
             res_Q_val = -math.inf
-            for i in self.childrens:
-                Q_val = self.agent.getQ(self, observation, self.childrens[i])
+            for i in self.children:
+                Q_val = self.agent.getQ(self, observation, self.children[i])
                 if res is None or Q_val > res_Q_val:
                     res_Q_val = Q_val
-                    res = self.childrens[i]
+                    res = self.children[i]
             return res
 
     def init_nodes(self):
+        """
+        Initializes the task graph for the Taxi problem
+
+        """
         primitive_move = []
+        MOVE_UP, MOVE_DOWN, MOVE_LEFT, MOVE_RIGHT, PICKUP, DROPOFF, \
+            NAVIGATE_R, NAVIGATE_G, NAVIGATE_Y, NAVIGATE_B, GET_PASS, PUT_PASS, ROOT = range(13)
+        MOVES = [MOVE_UP, MOVE_DOWN, MOVE_LEFT, MOVE_RIGHT]
+        NAVIGATES = [NAVIGATE_R, NAVIGATE_G, NAVIGATE_Y, NAVIGATE_B]
         for i in range(4):
-            primitive_move.append(self.Node(self, i, i, None))
-        primitive_pickup = self.Node(self, 4, 4, None)
-        primitive_dropoff = self.Node(self, 5, 5, None)
-        navigates = [self.Node(self, -1, 6 + i, self.check_navigate(i), primitive_move) for i in range(4)]
-        get = self.Node(self, -1, 10, self.check_pickup(), [primitive_pickup] + navigates)
-        put = self.Node(self, -1, 11, self.check_dropoff(), [primitive_dropoff] + navigates)
-        self.root = self.Node(self, -1, 12, self.check_done(), [get, put])
+            primitive_move.append(self.Node(self, MOVES[i], MOVES[i], None))
+        primitive_pickup = self.Node(self, PICKUP, PICKUP, None)
+        primitive_dropoff = self.Node(self, DROPOFF, DROPOFF, None)
+        navigates = [self.Node(self, -1, NAVIGATES[i], self.check_navigate(i), primitive_move) for i in range(4)]
+        get = self.Node(self, -1, GET_PASS, MAXQAgent.check_pickup, [primitive_pickup] + navigates)
+        put = self.Node(self, -1, PUT_PASS, MAXQAgent.check_dropoff, [primitive_dropoff] + navigates)
+        self.root = self.Node(self, -1, ROOT, MAXQAgent.check_done, [get, put])
 
     def MAXQ(self, current_node, observation, env):
         if self.log:
             print("in node {}, observation {}".format(current_node.id, self.taxiDecode(observation)))
         if current_node.is_primitive():
             next_observation, reward, done, _ = env.step(current_node.action)
-            self.V[(current_node.id, observation)] = self.V[(current_node.id, observation)] * (1 - self.LR) + \
-                self.LR * reward
+            self.V[current_node.id, observation] = self.V[current_node.id, observation] * (1 - self.LR) + \
+                                                   self.LR * reward
             if self.log:
                 env.render()
             if done:
@@ -182,19 +162,9 @@ class MAXQAgent(object):
                 N, next_observation = self.MAXQ(next_node, observation, env)
                 if next_observation == "DONE":
                     return (count + N, next_observation)
-                if (current_node.id, observation, next_node.id) not in self.C:
-                    self.C[(current_node.id, observation, next_node.id)] = -1
-                self.C[(current_node.id, observation, next_node.id)] = \
-                    (1 - self.LR) * self.C[(current_node.id, observation, next_node.id)] + \
+                self.C[current_node.id, observation, next_node.id] = \
+                    (1 - self.LR) * self.C[current_node.id, observation, next_node.id] + \
                     self.LR * (self.DF ** N) * self.getV(current_node, next_observation)
                 observation = next_observation
                 count += N
             return count, observation
-
-
-    def act(self, observation):
-        pass
-
-    def update_memory(self, observation, action, reward, next_observation, done):
-        pass
-
--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-from taxi import TaxiEnv
+from gym.envs.toy_text.taxi import TaxiEnv
 from agent import MAXQAgent as Agent
-# from agent import QLearningAgent as Agent
-# from agent import RandomAgent as Agent
+
 
 class RunAgent():
     def __init__(self):
@@ -10,42 +9,20 @@ class RunAgent():
         self.max_step = 100000
         self.render = True
 
-    def do_episode(self):
-        observation = self.env.reset()
-        reward, done = 0, False
-        amount = 0
-        for step_i in range(self.max_step):
-            if self.render:
-                self.env.render()
-            action = self.agent.act(observation)
-            next_observation, reward, done, _ = self.env.step(action)
-            self.agent.update_memory(observation, action, reward, next_observation, done)
-            amount += 1
-            if reward > 0:
-                print(reward)
-            if done:
-                break
-        return amount
-
     def run_maxQ(self):
         return self.agent.MAXQ(self.agent.root, self.env.reset(), self.env)
 
 
-
-ra = RunAgent()
-# ra.agent.log = True
-ra.agent.load_memory()
-# ra.agent.RF = 0.1
-cnt = 0
-try:
+if __name__ == "__main__":
+    ra = RunAgent()
+    # ra.agent.log = True
+    ra.agent.load_memory()
+    ra.agent.RF = 0.1
+    cnt = 0
     while True:
         ra.agent.LR *= 0.9999
-        # ra.agent.RF *= 0.999
+        ra.agent.RF *= 0.999
         print(cnt, ra.run_maxQ(), ra.agent.RF)
         ra.agent.save_memory()
         cnt += 1
-        break
-
-except Exception as e:
-    print('Terminate')
-    ra.agent.save_memory()
+        # break
